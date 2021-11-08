c9f23723b01143e08664c6f2e40d2dc90692654d
Daviani <contact@daviani.dev>
2021-11-08 15:05:55 +0100

    [CONFIGURE] configure eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: [
+    'standard'
+  ],
+  rules: {
+    semi: [2, 'always'],
+    'space-before-function-paren': ['error', {
+      anonymous: 'always',
+      named: 'never',
+      asyncArrow: 'always'
+    }]
+  }
+}
